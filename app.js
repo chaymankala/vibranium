9b7d0e1fcdea1e9cef64088e6db3463025f61a56
ChaitanyaMankala <chaithanya.mankala@gmail.com>
2017-11-16 00:51:14 +0530

    new route

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var category = require('./routes/category');
-var wallpaper = require('./routes/wallpaper')
+var wallpaper = require('./routes/wallpaper');
+var test = require('./routes/test');
 
 var mongo = require('./database.json');
 var bodyParser = require('body-parser');
@@ -38,6 +39,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/category', category)
 app.use('/wallpaper', wallpaper)
+app.use('/test', test);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
